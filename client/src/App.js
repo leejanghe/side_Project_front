d1591410e2c4c5058cf53d100f843f4a7d00b0e6
leejanghe <babybye777@gmail.com>
2022-11-08 23:22:59 +0900

    서버연결 태스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,49 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState("전송실패");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/hello")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const testbtnA = () => {
+    axios
+      .get("http://localhost:8080/api/pet")
+      .then((res) => {
+        console.log("a", res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const testbtnB = () => {
+    axios
+      .get("http://localhost:8080/api/hello")
+      .then((res) => {
+        console.log("b", res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <h1>test</h1>
+      <h1>{data}</h1>
+      <button onClick={testbtnA}>a요청!</button>
+      <button onClick={testbtnB}>b요청!</button>
     </div>
   );
 }
